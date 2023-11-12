6784415e97072447cb09d073d2ddbdff67e6afb0
João Pedro Marins <136388066+joaopbmarins@users.noreply.github.com>
2023-11-12 12:53:08 -0300

    Update matricula.c

--- ep/1/matricula.c
+++ ep/1/matricula.c
@@ -18,43 +18,6 @@
     return -1;
 }
 
-void merge(char array[][31],  int l,  int r){
-    char temp[l+r+1][31];
-    for( int i=l;i<=r;i++){
-        strcpy(temp[i], array[i]);
-    }
-     int m = (l+r)/2;
-     int i1 = l, i2 = m+1;
-    for( int curr=l;curr<=r;curr++){
-        if(i1==m+1){
-            strcpy(array[curr], temp[i2]);
-            i2++;
-        }
-        else if(i2 > r){
-            strcpy(array[curr], temp[i1]);
-            i1++;
-        }
-        else if(strcmp(temp[i1], temp[i2])<=0){
-            strcpy(array[curr], temp[i1]);
-            i1++;
-        }
-        else{
-            strcpy(array[curr], temp[i2]);
-            i2++;
-        }
-    }    
-}
-
-void mergesort(char array[][31],  int l,  int r){
-    if(l<r){
-        int m = (l+r)/2;
-        mergesort(array, l, m);
-        mergesort(array, m+1, r);
-        merge(array, l , r);
-    }
-}
-
-
 int main(){
      int n;
     
@@ -64,8 +27,6 @@ int main(){
     for( int i=0;i<n;i++){
         scanf(" %s\n", alunos[i]);
     }
-
-    //mergesort(alunos, 0 , n-1);
 
      int q;
     char aluno_desejado[31];
