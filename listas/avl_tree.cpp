d8ecf872d7cab39a4d0e88b2f0b6bd93674bd076
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2024-01-07 02:34:09 -0300

    alguma tentativa de avl_tree

--- listas/avl_tree.cpp
+++ listas/avl_tree.cpp
@@ -8,6 +8,7 @@ typedef struct BSTNode{
     int key;
     int element;
     int height;
+    int rank;
     struct BSTNode *left;
     struct BSTNode *right;
 }BSTNode;
@@ -18,22 +19,8 @@ BSTNode *CreateBSTnode(int k, int e){
     node->key = k;
     node->left = node->right = NULL;
     node->height = 0;
+    node->rank = 1;
     return node;
-}
-
-BSTNode *getmin(BSTNode *rt){
-    if(rt->left == NULL){
-        return rt;
-    }
-    return getmin(rt->left);
-}
-
-BSTNode *deletemin(BSTNode *rt){
-    if(rt->left == NULL){
-        return rt->right;
-    }
-    rt->left = deletemin(rt->left);
-    return rt;
 }
 
 int h(BSTNode *rt){
@@ -62,23 +49,57 @@ BST *CreateBST(){
     return bst;
 }
 
-int findhelp(BSTNode *rt, int k){
+BSTNode *findhelp(BSTNode *rt, int k){
     if(rt == NULL){
-        return -1;//erro arvore nula
+        return NULL;//erro arvore nula
     }
     if(rt->key > k){
         return findhelp(rt->left, k);
     }
     else if(rt->key == k){
-        return rt->element;
+        return rt;
     }
     else{
         return findhelp(rt->right, k);
     }
 }
 
-int Find(BST *bst, int k){
-    return findhelp(bst->root, k);
+/*
+int tmp = 1;
+void RankHelp(BSTNode *rt){
+    if(rt->left == NULL){
+        return;
+    }
+    else{
+        tmp++;
+        RankHelp(rt->left);
+    }
+    if(rt->right == NULL){
+        return;
+    }
+    else{
+        tmp++;
+        RankHelp(rt->right);
+    }
+}
+
+void Rank(BSTNode *rt){
+    tmp = 1;
+    if(rt->left != NULL){
+        tmp++;
+        RankHelp(rt->left);
+    }
+    else if(rt->right != NULL){
+        tmp++;
+        RankHelp(rt->right);
+    }
+}*/
+
+int Rank(BSTNode *rt){
+    if(rt == NULL){
+        return 0;
+    }
+    return rt->rank;
 }
 
 BSTNode *rightRotate(BSTNode *rt){
@@ -88,6 +109,8 @@ BSTNode *rightRotate(BSTNode *rt){
     rt->left = lr;
     rt->height = max(h(rt->left), h(rt->right)) + 1;
     l->height = max(h(l->left), h(l->right)) + 1;
+    rt->rank= Rank(rt->left) + Rank(rt->right) +1;
+    l->rank = Rank(l->left) + Rank(l->right) +1;
     return l;
 }
 
@@ -98,6 +121,8 @@ BSTNode *leftRotate(BSTNode *rt){
     rt->right = rl;
     rt->height = max(h(rt->left), h(rt->right)) + 1;
     r->height = max(h(r->left), h(r->right)) + 1;
+    rt->rank= Rank(rt->left) + Rank(rt->right) +1;//
+    r->rank = Rank(r->left) + Rank(r->right) +1;  //Rank(rt) +1;//Rank(r->left) + Rank(r->right)
     return r;
 }
 
@@ -106,11 +131,14 @@ BSTNode *inserthelp(BSTNode *rt, int k, int e){
         return CreateBSTnode(k, e);
     }
     if(rt->key > k){
+        rt->rank = Rank(rt) +1;//toda vez q eu navego para esquerda preciso adicionar um no rank
         rt->left = inserthelp(rt->left, k, e);
     }
     else{
         rt->right = inserthelp(rt->right, k , e);
     }
+
+    //rt->rank = Rank(rt) +1;//+ Rank(rt->right) 
 
     rt->height = 1 + max(h(rt->left), h(rt->right));
     int balance = getBalance(rt);
@@ -136,53 +164,15 @@ void Insert(BST *bst, int key, int element){
     bst->node_count++;
 }
 
-BSTNode *removehelp(BSTNode *rt, int k){
-    if(rt == NULL){
-        return NULL;
-    }
-    if(rt->key > k){
-        rt->left = removehelp(rt->left, k);
-    }
-    else if(rt->key < k){
-        rt->right = removehelp(rt->right, k);
-    }
-    else{
-        if(rt->left == NULL){
-            return rt->right;
-        }
-        else if(rt->right == NULL){
-            return rt->left;
-        }
-        else{
-            BSTNode *tmp = getmin(rt->right);
-            rt->element = tmp->element;
-            rt->key = tmp->key;
-            rt->right = deletemin(rt->right);
-        }
-    }
-    return rt;
-}
-
-int Remove(BST *bst, int key){
-    int tmp = findhelp(bst->root, key);
-    if(tmp != -1){//teste para dizer se a busca funcionou
-        bst->root = removehelp(bst->root, key);
-        bst->node_count--;
-    }
-    return tmp;
-}
-
 int posi = 0;
-//vector<int> array;
 
 void InOrder(BSTNode *rt, int key, bool &Achou){
     if(rt != NULL && !Achou){
         InOrder(rt->left, key, Achou);
+        if(Achou) return;
         posi++;
-        //array.push_back(rt->key);
         if(rt->key == key){
             Achou = true;
-            cout << posi << endl;
             return;
         }
         InOrder(rt->right, key, Achou);
@@ -190,32 +180,39 @@ void InOrder(BSTNode *rt, int key, bool &Achou){
 }
 
 
+
 int main(){
 
     BST *bst = CreateBST();
     int n;
     scanf("%d", &n);
-    for(int i=0;i<n;i++){
+    while(n>0){
         int oper, value;
         scanf("%d %d", &oper, &value);
+
         if(oper == 1){//insert
             Insert(bst, value, value);
         }
+
         else if(oper == 2){//find
             bool Achou = false;
-            InOrder(bst->root, value, Achou);
-            //remover dps
-            //for(auto i: array) cout << i << " ";
-            //cout << endl;
-            //array.clear();
-            //
-            if(!Achou){
+            BSTNode *node = findhelp(bst->root, value);
+            if(node != NULL){
+                InOrder(bst->root, value, Achou);
+                //cout << posi << endl;
+                cout << node->rank << endl;
+                if(posi != node->rank){
+                    cout << "algo está errado" << endl;
+                }
+            }
+            else{
                 cout << "Data tidak ada" << endl;
             }
             posi = 0;
         }
+        n--;
     }
-    
+
 
     return 0;
 }
