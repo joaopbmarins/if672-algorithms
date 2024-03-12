26a639db33134ebdefb3054ac3241aeb88d281ce
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2024-03-12 13:19:46 -0300

    update Spiky_Mazes

--- listas/Spiky_Mazes.cpp
+++ listas/Spiky_Mazes.cpp
@@ -8,7 +8,6 @@ bool deu_bom = false;
 bool travessia(int i, int j, int avoid, vector<vector<char>> &matriz, vector<vector<bool>> &visitados){
     if(i < 0 || j < 0 || i >= row || j >= columns || matriz.at(i).at(j) == '#' || visitados.at(i).at(j)) return false;
 
-    //cout << matriz.at(i).at(j) << endl;
     visitados.at(i).at(j) = true; 
 
 
@@ -29,7 +28,6 @@ int main(){
     cin >> row >> columns >> avoid_o;
 
     pair <int, int> jarmtin_pos;
-    pair <int, int> treasure_pos;
 
     vector<vector<char>> matriz;
     vector<vector<bool>> visitados;
@@ -43,7 +41,6 @@ int main(){
             cin >> tmp;
             matriz.at(i).at(j) = tmp;
             if(tmp == '@') jarmtin_pos = {i,j};
-            if(tmp == 'x') treasure_pos = {i,j};
         }
     }
 
@@ -56,19 +53,6 @@ int main(){
         cout << "SUCCESS\n";
     else
         cout << "IMPOSSIBLE\n";
-
-
-    /*
-    for(auto i : matriz){//imprime matriz
-        for(auto j : i){
-            cout << j;
-        }
-        cout << endl;
-    }
-    */
-
-    //cout << jarmtin_pos.first << " " << jarmtin_pos.second << endl;
-    //cout << treasure_pos.first << " " << treasure_pos.second << endl;
 
     return 0;
 }
