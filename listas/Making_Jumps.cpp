341f9466ac4a3651121a4ab9034086a33416b371
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2024-03-15 15:34:40 -0300

    Making_Jumps recursão ajeitada e sem strcpy

--- listas/Making_Jumps.cpp
+++ listas/Making_Jumps.cpp
@@ -2,24 +2,22 @@
 using namespace std;
 #define endl "\n"
 
-int best = 0;
-void travessia(int i, int j, int qtd, vector<vector<int>> &matriz, vector<vector<bool>> &visitados){
+
+void travessia(int i, int j, int qtd, int &best, vector<vector<int>> &matriz, vector<vector<bool>> &visitados){
     if(i < 0 || j < 0 || i >= 10 || j >= 10 || matriz.at(i).at(j) == 0 || visitados.at(i).at(j)) return;
 
     visitados.at(i).at(j) = true;
     qtd++;
     if(qtd > best) best = qtd;
 
-    //(r-2,c-1), (r-2,c+1), (r-1,c-2), (r-1,c+2), (r+1,c-2), (r+1,c+2), (r+2,c-1), or (r+2,c+1)
-
-    travessia(i-2, j-1, qtd, matriz, visitados); 
-    travessia(i-2, j+1, qtd, matriz, visitados);
-    travessia(i-1, j-2, qtd, matriz, visitados);
-    travessia(i-1, j+2, qtd, matriz, visitados);
-    travessia(i+1, j-2, qtd, matriz, visitados);
-    travessia(i+1, j+2, qtd, matriz, visitados);
-    travessia(i+2, j-1, qtd, matriz, visitados);
-    travessia(i+2, j+1, qtd, matriz, visitados);
+    travessia(i-2, j-1, qtd, best, matriz, visitados); 
+    travessia(i-2, j+1, qtd, best, matriz, visitados);
+    travessia(i-1, j-2, qtd, best, matriz, visitados);
+    travessia(i-1, j+2, qtd, best, matriz, visitados);
+    travessia(i+1, j-2, qtd, best, matriz, visitados);
+    travessia(i+1, j+2, qtd, best, matriz, visitados);
+    travessia(i+2, j-1, qtd, best, matriz, visitados);
+    travessia(i+2, j+1, qtd, best, matriz, visitados);
 
     visitados.at(i).at(j) = false;
 
@@ -56,8 +54,8 @@ int main(){
             }
         }
 
-        int qtd=0;
-        travessia(0,0, qtd, matriz, visitados);
+        int qtd=0, best=0;
+        travessia(0,0, qtd, best, matriz, visitados);
 
         for(int i=0;i<10;i++){
             for(int j=0;j<10;j++){
@@ -65,17 +63,13 @@ int main(){
             }
         }
 
-        char quadrado_fdp[10];
-
         if(casas_max - best == 1){
-            stpcpy(quadrado_fdp, "square");
+            printf("Case %d, %d square can not be reached.\n", caso, casas_max - best);
         }
         else{
-            stpcpy(quadrado_fdp, "squares");
+            printf("Case %d, %d squares can not be reached.\n", caso, casas_max - best);
         }
 
-        printf("Case %d, %d %s can not be reached.\n", caso, casas_max - best, quadrado_fdp);
-        best = 0;
         cin >> size;
     }
 
